8414eb168b12b3445298c6744830b9bf0cb0ca17
cbath <craigbath@outlook.com>
2023-11-26 22:41:49 -0500

    testing

---
+++ tests/reports/pytest.md
@@ -0,0 +1,19 @@
+# Test Report
+
+*Report generated on 26-Nov-2023 at 22:41:38 by [pytest-md]* 📝
+
+[pytest-md]: https://github.com/hackebrot/pytest-md
+
+## Summary
+
+2 tests ran in 1.01 seconds ⏱
+
+- 2 passed 😃
+
+## 2 passed 😃
+
+### tests/test.py
+
+`test_get_event_info` 0.00s ⏱
+
+`test_get_comment_tag` 0.00s ⏱
--- tests/test.py
+++ tests/test.py
@@ -14,16 +14,23 @@ SCRIPT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0,  os.path.abspath(f'{SCRIPT_DIRECTORY}/../src'))
 print(f"Loading module '{SCRIPT_DIRECTORY}'")
 
-from main import get_event_info
+from main import get_event_info, get_comment_tag
 
 #=============================================================================#
 # Test files
 #=============================================================================#
 EVENT_PATH_VALID=f"{SCRIPT_DIRECTORY}/data/pr_event.json"
 EVENT_PATH_INVALID=f"{SCRIPT_DIRECTORY}/data/non_pr_event.json"
+COMMENT = 'Well. Hellow there'
+COMMENT_ID = random.randint(0, 10000)
 
-#TODO - create test PR from test branch
+# TODO - create test PR from test branch
 
+
+
+#=============================================================================#
+# Tests
+#=============================================================================#
 def test_get_event_info():
     info = get_event_info(EVENT_PATH_VALID)
     good_type = isinstance(info,dict)
@@ -32,3 +39,9 @@ def test_get_event_info():
     values_expected = ['spicyparrot', 'refs/heads/testing-setup', 'testing-setup', 'spicyparrot:testing-setup', 'pr-comment', 'spicyparrot/pr-comment']
     good_values = values_expected == list(info.values())
     assert all([good_type,good_keys,good_values]), "Invalid dictionary parsed from event"
+
+
+def test_get_comment_tag():
+    tag = get_comment_tag(COMMENT_ID)
+    hidden = tag.startswith('[comment]:')
+    assert hidden, "Invalid comment tag generated"
